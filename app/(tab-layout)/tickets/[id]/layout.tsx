d9c90b7285e00f8f61d07655f66ab408f8091916
shaddollxz <shaddollxz@163.com>
2024-09-20 17:06:13 +0800

    fixup! fix: route

--- app/(tab-layout)/tickets/[id]/layout.tsx
+++ app/(tab-layout)/tickets/[id]/layout.tsx
@@ -1,16 +1,10 @@
-import type { ReactNode } from "react";
+import type { ReactNode } from 'react'
 
-export default function IdLayout({
-  children,
-  modal,
-}: {
-  children: ReactNode;
-  modal: ReactNode;
-}) {
+export default function IdLayout({ children, modal }: { children: ReactNode; modal: ReactNode }) {
   return (
     <div className="flex">
       {children}
       {modal}
     </div>
-  );
+  )
 }
--- app/(tab-layout)/tickets/[id]/page.tsx
+++ app/(tab-layout)/tickets/[id]/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client'
 
-import { useParams, useRouter } from "next/navigation";
+import { useParams, useRouter } from 'next/navigation'
 
-import { TicketList } from "../ticket-list";
+import { TicketList } from '../ticket-list'
 
 // shareable detail page partial
 export default function TicketDetailPage() {
-  const router = useRouter();
-  const params = useParams();
+  const router = useRouter()
+  const params = useParams()
 
   return (
     <>
@@ -18,11 +18,11 @@ export default function TicketDetailPage() {
         <p>{`I'm the list item of ${params.id}`}</p>
         <button
           className="px-4 py-2 rounded-sm bg-blue-700 text-white"
-          onClick={() => router.push("/tickets")}
+          onClick={() => router.push('/tickets')}
         >
           back to list
         </button>
       </div>
     </>
-  );
+  )
 }
